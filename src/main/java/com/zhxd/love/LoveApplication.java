32e48d57ffeb2e981d266dfc27a4ab90a1e35504
hyh <hyh142322@163.com>
2022-03-14 14:38:06 +0800

    提交

--- src/main/java/com/zhxd/love/LoveApplication.java
+++ src/main/java/com/zhxd/love/LoveApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class LoveApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LoveApplication.class, args);
     }
